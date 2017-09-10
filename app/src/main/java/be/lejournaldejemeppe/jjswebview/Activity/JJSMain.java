44a83e0e8ce062e0af586e8337d2370737ddb33a
LotuxPunk <clement@vandendaelen.com>
2017-09-11 00:02:51 +0200

    init

---
+++ app/src/main/java/be/lejournaldejemeppe/jjswebview/Activity/JJSMain.java
@@ -0,0 +1,169 @@
+package be.lejournaldejemeppe.jjswebview.Activity;
+
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import be.lejournaldejemeppe.jjswebview.R;
+import be.lejournaldejemeppe.jjswebview.Others.ViewClient;
+
+public class JJSMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private WebView mWebView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jjsmain);
+
+        //Get Swipe Addon
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
+        //Get WebView
+        mWebView = (WebView)findViewById(R.id.activity_main_webview);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        if ( !isNetworkAvailable() ) { // loading offline
+            mWebView.loadUrl("file:///android_asset/index.html");
+        } else {
+            mWebView.loadUrl("http://lejournaldejemeppe.be");
+        }
+        mWebView.setWebViewClient(new ViewClient(){
+            //Desactive la jolie boucle après chargement
+            public void onPageFinished(WebView view, String url) {
+                setSwipeFalse();
+            }
+        });
+
+        //set le reload de la webview
+        mSwipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        mWebView.reload();
+                    }
+                });
+
+        //Truc par défaut
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {loadShare();}
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    /*@Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }*/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.jjsmain, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            goHome();
+        } else if (id == R.id.nav_contact) {
+            goContact();
+        } else if (id == R.id.nav_share) {
+            loadShare();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService( CONNECTIVITY_SERVICE );
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+    public void goHome(){
+        mWebView.loadUrl("http://lejournaldejemeppe.be");
+        mWebView.setWebViewClient(new ViewClient());
+    }
+
+    public void goContact(){
+        mWebView.loadUrl("http://lejournaldejemeppe.be/nous-contacter/");
+        mWebView.setWebViewClient(new ViewClient());
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(mWebView.canGoBack()){
+            mWebView.goBack();
+        }
+    }
+
+    public void loadShare(){
+        String url = mWebView.getUrl();
+        String title = mWebView.getTitle();
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_SUBJECT, title);
+        i.putExtra(Intent.EXTRA_TEXT, url);
+        startActivity(Intent.createChooser(i, "Partager"));
+    }
+
+    public void setSwipeFalse(){
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+}
---
+++ app/src/main/java/be/lejournaldejemeppe/jjswebview/Others/ViewClient.java
@@ -0,0 +1,29 @@
+package be.lejournaldejemeppe.jjswebview.Others;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by LotuxPunk on 05/09/2017.
+ */
+
+public class ViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url){
+        if (Uri.parse(url).getHost().endsWith("lejournaldejemeppe.be")) {
+            return false;
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+        view.getContext().startActivity(intent);
+
+        return true;
+    }
+
+    @Override
+    public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+        // TODO Auto-generated method stub
+        view.loadUrl("file:///android_asset/index.html");
+    }
+}
