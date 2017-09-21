6c856b6901a7542d14b2d1c0ab8685ae47ad65d3
LotuxPunk <clement@vandendaelen.com>
2017-09-21 08:26:29 +0200

    reset

---
+++ app/src/main/java/be/lejournaldejemeppe/jjswebview/JjsApp.java
@@ -0,0 +1,15 @@
+package be.lejournaldejemeppe.jjswebview;
+
+import android.app.Application;
+
+/**
+ * Created by LotuxPunk on 29/07/2017.
+ */
+
+public class JjsApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
